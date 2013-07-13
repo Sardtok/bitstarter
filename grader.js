03c9cb13f5d63f79066dd2ab64883f6c13ce68b3
Sigmund Hansen <sigmunha@ifi.uio.no>
2013-07-13 21:17:11 +0200

    Adding support for grading URLs.

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -44,15 +45,19 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtml = function(html, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
+        var present = html(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
     return out;
+};
+
+var checkHtmlFile = function(htmlfile, checksfile) {
+    $ = cheerioHtmlFile(htmlfile);
+    return checkHtml($, checksfile);
 };
 
 var clone = function(fn) {
@@ -61,14 +66,32 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var outputResults = function(checkJson) {
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+    return outJson;
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_url>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if (program.url) {
+        rest.get(program.url).on('complete', function(result) {
+            if (result instanceof Error) {
+                console.log('URL failz:', result.message);
+                process.exit(1);
+            }
+            var checkJson = checkHtml(cheerio.load(result), program.checks);
+            outputResults(checkJson);
+        });
+    } else {
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        outputResults(checkJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
